aa743c167000aa7f0dc89f9fe1847e53e66d088e
Tamer Moustafa <tamer.moustafa@transformuk.com>
2016-01-29 16:27:33 +0000

    RT-354 - Extract rt_emailer out of caseworker

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./activator assembly
-java -jar ./target/scala-2.11/rt-emailer-assembly-1.0-SNAPSHOT.jar -c ./conf/application.conf
+java -jar ./target/scala-2.11/rt-emailer-assembly-1.0-SNAPSHOT.jar -c ./conf/rt-emailer.conf
