9ec178d013d264e9d39688177239bd8d95904664
Tamer Moustafa <tamer.moustafa@transformuk.com>
2016-01-29 11:22:17 +0000

    RT-354 - Extract rt_emailer out of caseworker

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ rtEmailerVersion=$(grep -i ^version ./version.properties | cut -d'=' -f 2)
 
 # Write app version and build number to conf/version.conf
 export build_number=${BUILD_NUMBER:-1}
-echo "rt-emailer-version=\""$rtIntegServiceVersion"-"$BUILD_NUMBER\" > src/main/resources/version.conf
+echo "rt-emailer-version=\""$rtEmailerVersion"-"$BUILD_NUMBER\" > src/main/resources/version.conf
 
 
 # Ensure working directory is local to this script
 cd "$(dirname "$0")"
 
-./activator clean compile test publish
+./activator clean update test assembly
 
 if [ $? -ne 0 ]; then
   echo "[build.sh] failure"
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -9,3 +9,5 @@ libraryDependencies += "com.typesafe" % "config" % "1.2.1"
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.0.0")
 
 addSbtPlugin("me.lessis" % "bintray-sbt" % "0.1.1")
+
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.0")
