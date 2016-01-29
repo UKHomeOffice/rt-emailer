a31bbbc4afebc34a4ca4d18eb02644d0fa09c211
Tamer Moustafa <tamer.moustafa@transformuk.com>
2016-01-29 12:17:24 +0000

    RT-354 - Extract rt_emailer out of caseworker

--- build_rpm.sh
+++ build_rpm.sh
@@ -2,4 +2,4 @@
 version="1.0.0"
 cd fpm
 cp ../target/scala-2.11/rt-emailer-assembly*.jar  build/usr/share/rt-emailer/rt-emailer.jar
-fpm -x .git* --config-files etc/rt-emailer/rt-emailer.conf --before-install scripts/preinstall.sh --after-install scripts/postinstall.sh --before-remove scripts/preuninstall.sh --after-remove scripts/postuninstall.sh -C build -t rpm -s dir -d java -n rt-emailer -v "$version-$BUILD_NUMBER" -a all .
+fpm -x .git* --config-files etc/rt-emailer/application.conf --before-install scripts/preinstall.sh --after-install scripts/postinstall.sh --before-remove scripts/preuninstall.sh --after-remove scripts/postuninstall.sh -C build -t rpm -s dir -d java -n rt-emailer -v "$version-$BUILD_NUMBER" -a all .
